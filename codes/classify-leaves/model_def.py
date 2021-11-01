85c80eb9c3db50009bd1c0c318dded6b3824065c
PXL <921417536@qq.com>
2021-11-01 16:51:58 +0800

    classify-leaves:add test

--- codes/classify-leaves/model_def.py
+++ codes/classify-leaves/model_def.py
@@ -4,11 +4,11 @@ import torchvision
 from lib.utils import *
 
 
-def Net(pthfile=None):
+def Net(pthfile=None, device='cuda'):
     if pthfile is None:
         model = torchvision.models.resnet18(pretrained=True)
         model.fc = nn.Linear(model.fc.in_features, 176)
     else:
         model = torchvision.models.resnet18()
-        model.load_state_dict(pt.load(pthfile))
+        model.load_state_dict(pt.load(pthfile, map_location=device))
     return model
