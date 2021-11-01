5439dab163285566ac9e3ba6a5a2bce67443e126
PenXLa <921417536@qq.com>
2021-11-01 18:44:17 +0800

    classify-leaves:limited test

--- codes/classify-leaves/test.py
+++ codes/classify-leaves/test.py
@@ -3,12 +3,13 @@ import pandas as pd
 from utils import *
 from lib.utils import *
 
-def test(model, device='cuda'):
+def test(model, device='cuda', test_num=-1):
     submission = pd.DataFrame(columns=['image', 'label'])
     lblenc, _, _, test_data, filenames = load_leaves()
     test_loader = DataLoader(test_data, 1, shuffle=False, num_workers=num_workers)
     with pt.no_grad():
         for i, imgs in enumerate(tqdm(test_loader, desc="test")):
+            if test_num != -1 and i >= test_num: break
             imgs = imgs.to(device)
             pred = model(imgs).argmax(dim=1).to('cpu')
             submission = submission.append({'image':filenames.iat[i], 'label':lblenc.inverse_transform(pred).item()}, ignore_index=True)
