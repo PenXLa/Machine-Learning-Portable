016b876b1f21e8b3499ee28ee36931872df7d215
PenXLa <921417536@qq.com>
2021-11-02 14:28:06 +0800

    classsify-leaves:evaluaion batch size

--- codes/classify-leaves/evaluate.py
+++ codes/classify-leaves/evaluate.py
@@ -14,7 +14,8 @@ def evaluate(model:nn.Module, device='cuda', eval_num:int=-1):
             if eval_num != -1 and i >= eval_num: break
             imgs = imgs.to(device)
             pred = model(imgs).argmax(dim=1).to('cpu')
+            pred = lblenc.inverse_transform(pred)
             for ans in pred:
-                submission = submission.append({'image':filenames.iat[sample_id], 'label':lblenc.inverse_transform(ans).item()}, ignore_index=True)
+                submission = submission.append({'image':filenames.iat[sample_id], 'label':ans}, ignore_index=True)
                 sample_id += 1
     submission.to_csv(mksure(data_drive / "classify-leaves/submission.csv"), index=None)
